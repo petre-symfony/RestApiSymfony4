12023f4306cf8a4178ae975f02cceec57e2bdb1e
petrero <petrero@gmail>
2019-01-11 10:51:51 -0600

    Modify user fixture to encode password Modify project fixture Run: bin/console doctrine:fixtures:load

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,15 +2,31 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends Fixture
 {
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder) {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user->setUsername('weaverryan');
+        $user->setEmail('ryan@knplabs.com');
+        $plainPassword = 'foo';
+        $encoded = $this->encoder->encodePassword($user, $plainPassword);
+        $user->setPassword($encoded);
+        $roles = [];
+        $roles[] = 'ROLE_USER';
+        $user->setRoles($roles);
+        $manager->persist($user);
 
         $manager->flush();
     }
