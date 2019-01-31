cd5231ec60cda706087d4835fa54a7ecaf299877
petrero <petrero@gmail>
2019-01-31 11:26:54 -0600

    Serialize every property of the resource even when they ave null value

--- src/Controller/APIBaseController.php
+++ src/Controller/APIBaseController.php
@@ -1,10 +1,10 @@
 <?php
 namespace App\Controller;
 
-
 use JMS\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use JMS\Serializer\SerializationContext;
 
 class APIBaseController extends AbstractController {
 	private $serializer;
@@ -14,7 +14,10 @@ class APIBaseController extends AbstractController {
 	}
 
 	protected function serialize($data){
-		return $this->serializer->serialize($data, 'json');
+		$context = new SerializationContext();
+		$context->setSerializeNull(true);
+		
+		return $this->serializer->serialize($data, 'json', $context);
 	}
 
 	protected function createAPIResponse($data, $statusCode = 200){
--- tests/Controller/Api/ProgrammerControllerTest.php
+++ tests/Controller/Api/ProgrammerControllerTest.php
@@ -42,7 +42,8 @@ class ProgrammerControllerTest extends ApiTestCase {
 		$this->asserter()->assertResponsePropertiesExist($response, [
 			'nickname',
 			'avatarNumber',
-			'powerLevel'
+			'powerLevel',
+			'tagLine'
 		]);
 		$this->asserter()->assertResponsePropertyEquals($response, 'nickname', 'UnitTester');
 	}
